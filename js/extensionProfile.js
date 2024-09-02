4e3cf82821989e24f7ccb028be86d54cec0b905a
thehungrycoder225 <cyber_sky09@yahoo.com>
2024-09-03 06:02:01 +0800

    integrated extension services

---
+++ js/extensionProfile.js
@@ -0,0 +1,26 @@
+function displayTable() {
+  fetch('../data/extension_project-ds.JSON')
+    .then((response) => response.json())
+    .then((data) => {
+      const tableBody = document.querySelector('#extensionTable tbody');
+      data.forEach((item) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+                <td>${item.id}</td>
+                <td>${item.title}</td>
+                <td>${item.start_date}</td>
+                <td>${item.end_date}</td>
+                <td>${item.hours}</td>
+                <td>${item.multiplier}</td>
+                <td>${item.participants}</td>
+                <td>${item.total_score}</td>
+                <td>${item.final_score}</td>
+            `;
+        tableBody.appendChild(row);
+      });
+      new DataTable(document.querySelector('#extensionTable'));
+    })
+    .catch((error) => console.error('Error fetching data:', error));
+}
+
+displayTable();
