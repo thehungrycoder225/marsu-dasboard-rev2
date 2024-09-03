61f3c3bc25464346059600fbefc914fd66d24bdb
thehungrycoder225 <cyber_sky09@yahoo.com>
2024-09-03 13:04:11 +0800

    partial migration of research ds

--- js/researches.js
+++ js/researches.js
@@ -1,161 +1,37 @@
-const researchFacultyParent = document.querySelector(
-  '.research-faculty-parent'
-);
-
-const researchStudentParent = document.querySelector(
-  '.research-student-parent'
-);
-
-const renderFacultyResearch = (ds, parentElement) => {
-  const renderData = document.createElement('tr');
-  renderData.classList.add('fs-7');
-  renderData.innerHTML = `
-      <td class="fw-bold fw-italic col-md-3">${ds.title}</td>
-      <td class="col-md-2"> <ul class="list-unstyled" >
-      ${ds.authors.map((item) => `<li>${item}</li>`).join('')}
-     </ul></td>
-     <td class="col-md-2">${ds.conference}</td>
-     <td class="col-md-1 text-capitalize">${ds.category}</td>
-     <td class="col-md-2">${ds.year}</td>
-      `;
-  parentElement.appendChild(renderData);
-};
-
-const renderStudentResearch = (ds, parentElement) => {
-  const renderData = document.createElement('tr');
-  renderData.classList.add('fs-7');
-  renderData.innerHTML = `
-        <td class="col-md-2" >${ds.title}</td>
-        <td class="col-md-1" >${ds.year}</td>
-
-       <td class="col-md-2" > <ul class="list-unstyled">
-       ${ds.authors.map((item) => `<li>${item}</li>`).join('')}
-      </ul></td>
-      <td class="col-md-5" >${ds.abstract}</td>
-        `;
-  parentElement.appendChild(renderData);
-};
-
-const fetchResearchData = () => {
-  fetch('../data/research-ds.json')
+function displayResearchProfile() {
+  fetch('../data/research-ds.JSON')
     .then((response) => response.json())
     .then((data) => {
-      data.forEach((category) => {
-        if (category.faculty) {
-          category.faculty.forEach((ds) => {
-            renderFacultyResearch(ds, researchFacultyParent);
-          });
-        }
-        if (category.student) {
-          category.student.forEach((ds) => {
-            renderStudentResearch(ds, researchStudentParent);
-          });
-        }
+      console.log(data);
+      const tableBody = document.querySelector('#researchTable tbody');
+
+      data.forEach((item) => {
+        const researchers = Array.isArray(
+          item['Name of Faculty Researcher / Author']
+        )
+          ? item['Name of Faculty Researcher / Author'].join(', ')
+          : item['Name of Faculty Researcher / Author'] || 'N/A';
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${item['Research Title']}</td>
+          <td>${researchers}</td>
+          <td>${item['Status of Research']}</td>
+          <td>${item['Venue']}</td>
+          <td>${item['Duration of Research']['Date Started']}</td>
+          <td>${item['Duration of Research']['Date of Completion']}</td>
+          <td>${item['Product Name / Methods / Process / Technology']}</td>
+        `;
+        tableBody.appendChild(row);
       });
+
+      new DataTable('#researchTable', {
+        responsive: true,
+      });
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
     });
-};
+}
 
-const renderFacultyChart = () => {
-  let data = [
-    {
-      data: [7, 1, 15],
-      labels: ['International', 'National', 'Regional'],
-      backgroundColor: [
-        'rgba(251, 170, 48,1)',
-        'rgba(26, 166, 5, 1)',
-        'rgba(91, 0, 0,1)',
-      ],
-    },
-  ];
-
-  let options = {
-    responsive: true,
-    maintainAspectRatio: true,
-    legend: { position: 'bottom' },
-    // indexAxis: 'y',
-    plugins: {
-      legend: {
-        position: 'bottom',
-        display: false,
-      },
-      tooltip: {
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'Summary of Paper Presentations of ITE Faculty',
-      },
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  };
-
-  const ctx = document.getElementById('facultyChart').getContext('2d');
-  let facultyBarChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      datasets: data,
-      labels: data[0].labels,
-    },
-    options: options,
-  });
-};
-
-renderFacultyChart();
-
-const renderStudentChart = () => {
-  let data = [
-    {
-      data: [7, 3, 9],
-      labels: ['2020', '2021', '2022'],
-      backgroundColor: [
-        'rgba(251, 170, 48,1)',
-        'rgba(26, 166, 5, 1)',
-        'rgba(91, 0, 0,1)',
-      ],
-    },
-  ];
-
-  let options = {
-    responsive: true,
-    maintainAspectRatio: true,
-    legend: { position: 'bottom' },
-    indexAxis: 'y',
-    plugins: {
-      legend: {
-        position: 'bottom',
-        display: false,
-      },
-      tooltip: {
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'Summary of Student Researches from 2020 - 2022',
-      },
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  };
-
-  const ctx = document.getElementById('studentChart').getContext('2d');
-  let studentBarChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      datasets: data,
-      labels: data[0].labels,
-    },
-    options: options,
-  });
-};
-
-renderStudentChart();
-fetchResearchData();
-renderFacultyResearch();
-renderStudentResearch();
+document.addEventListener('DOMContentLoaded', displayResearchProfile);
