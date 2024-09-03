3da4ebb3aea70820299a201b8c5349e6d41e5c2a
thehungrycoder225 <cyber_sky09@yahoo.com>
2024-09-03 08:03:44 +0800

    modified extension profile

--- js/extensionProfile.js
+++ js/extensionProfile.js
@@ -18,9 +18,33 @@ function displayTable() {
             `;
         tableBody.appendChild(row);
       });
-      new DataTable(document.querySelector('#extensionTable'));
+      new DataTable(document.querySelector('#extensionTable'), {
+        searchable: true,
+        sortable: true,
+      });
     })
     .catch((error) => console.error('Error fetching data:', error));
 }
 
 displayTable();
+
+function displayTable2() {
+  fetch('../data/extension-ds.JSON')
+    .then((response) => response.json())
+    .then((data) => {
+      const tableBody = document.querySelector('#extensionTable2 tbody');
+      data.forEach((item) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${item['Mandated / Priority Program/s']}</td>
+          <td>${item['Extension Program']}</td>
+          <td>${item['Start Date (MMM-DD-YYYY)']}</td>
+          <td>${item['End Date (MMM-DD-YYYY)']}</td>
+        `;
+        tableBody.appendChild(row);
+      });
+      new DataTable(document.querySelector('#extensionTable2'), {});
+    });
+}
+
+displayTable2();
