6bf04b35eaabbbcf0ac3bee1c6829a4d2a9014ca
Dane <golightning225@gmail.com>
2025-04-21 12:40:00 +0800

    modified vite-config.js

--- marsu-pwa-dashboard/vite.config.js
+++ marsu-pwa-dashboard/vite.config.js
@@ -1,42 +1,56 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import flowbiteReact from 'flowbite-react/plugin/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    registerType: 'autoUpdate',
-    workbox: {
-      clientsClaim: true,
-      skipWaiting: true
-    },
-    injectRegister: false,
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        cleanupOutdatedCaches: true,
+        sourcemap: true,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'document',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'documents',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+              },
+            },
+          },
+        ],
+      },
+      injectRegister: false,
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'marsu-pwa-dashboard',
-      short_name: 'marsu-pwa-dashboard',
-      description: 'marsu-pwa-dashboard',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: 'MarSU Dashboard',
+        short_name: 'MarSU Dashboard',
+        description: 'MarSU Dashboard',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
-
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  }), flowbiteReact()],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+    flowbiteReact(),
+  ],
+});
